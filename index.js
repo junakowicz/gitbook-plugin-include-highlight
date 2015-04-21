492ea4eafa58c8933d925e6939775b06eb77d5ca
Andrzej Wower <andrzej.wower@youview.com>
2015-04-21 11:12:33 +0100

    package.json updated, removal of some comments

--- index.js
+++ index.js
@@ -1,79 +1,62 @@
-
 var path = require('path');
 var fs = require('fs');
 var Q = require('q');
 
 module.exports = {
 
-    hooks: {
+	hooks: {
+		"page:before": function(page) {
+			var re = /^!CODEFILE\s+(?:\"([^\"]+)\"|'([^']+)')\s*$/gm;
 
-        "page:before": function(page) {
-            // page.content is a string with the file markdown content
-	    // page.path is the path to the page in the gitbook
-            // page.rawPath is the path to the raw file
+			var dir = path.dirname(page.rawPath);
+			var makePath = function(filename) {
 
-	    // use multiline flag to grok every line, and global flag to 
-	    // find all matches -- finds '' and "" filenames
-	    // TODO: add initial \s* to eat up whitespace?
-	    var re = /^!CODEFILE\s+(?:\"([^\"]+)\"|'([^']+)')\s*$/gm;
-	    
-	    var dir = path.dirname(page.rawPath);
-	    // construct path from gitbook binary to target include
-	    var makePath = function(filename) {
+				return path.join(dir, filename);
+			};
 
-		return path.join(dir, filename);
-	    };
+			var getCodeLang = function(filepath) {
 
-		var getCodeLang = function(filepath) {
+				var lang ="";
+				var suffix = filepath.split(".").pop();
+				if(suffix=="feature")
+					lang="gherkin";
+				else
+				{
+					lang = suffix;
+				}
+				return lang;
 
-			var lang =""
-			var suffix = filepath.split(".").pop();
-			if(suffix=="feature")
-				lang="gherkin";
-			else
-			{
-				lang = suffix;
+				return path.join(dir, filename);
+			};
+
+			var files = {};
+			// return a closure for saving the passed text
+			var cacheFile = function(filepath) {
+				return function(text) {
+					files[filepath] = text;
+				};
+			};
+
+			var readFiles = []; // file promises
+
+			var res;
+			while (res = re.exec(page.content)) {
+
+				var filepath = makePath(res[1] || res[2]);
+				readFiles.push(
+					Q.nfcall(fs.readFile, filepath)
+						.then(cacheFile(filepath))
+				);
 			}
-
-			return lang;
-
-		return path.join(dir, filename);
-	    };
-	
-	    var files = {};
-	    // return a closure for saving the passed text
-	    var cacheFile = function(filepath) {
-		return function(text) {
-		    files[filepath] = text;
-		};
-	    };
-
-	    var readFiles = []; // file promises
-
-	    // find all !INCLUDE statements.
-	    // read and cache target files
-	    var res;
-	    while (res = re.exec(page.content)) {
-		
-		var filepath = makePath(res[1] || res[2]);
-		readFiles.push(
-		    Q.nfcall(fs.readFile, filepath)
-			.then(cacheFile(filepath))
-		);
-	    }
-	    // once all files are read, replace !INCLUDE statements with 
-	    // appropriate file content
-	    return Q.all(readFiles)
-		.then(function() {
-		    page.content = page.content.replace(re, function(match, p1, p2) {
-			var filepath = makePath(p1 || p2);
-			var codeLang = getCodeLang(filepath);
-			return "```"+codeLang+"\n" +files[filepath].toString().trim() +"\n```"; // strip whitespace
-
-
-		    });
-		    return page;
-		})
-        }
-    }
+			return Q.all(readFiles)
+				.then(function() {
+					page.content = page.content.replace(re, function(match, p1, p2) {
+						var filepath = makePath(p1 || p2);
+						var codeLang = getCodeLang(filepath);
+						return "```"+codeLang+"\n" +files[filepath].toString().trim() +"\n```"; // strip whitespace
+					});
+					return page;
+				})
+		}
+	}
 };
